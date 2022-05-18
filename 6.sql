56b2bebd0d8ef0a6ffb12d05ba307a89a145905d
IllyaMaz <maznichenkoo16@gmail.com>
2022-05-18 12:33:47 +0300

    debug

--- 6.sql
+++ 6.sql
@@ -3,5 +3,6 @@ from developer_project dp
 inner join developers d on dp.developer_id = d.id
 inner join projects p on dp.progect_id = p.id
 group by p.id
-order by sum(d.salary)/count(dp.developer_id) asc
+order by p.cost asc
 limit 1
+
